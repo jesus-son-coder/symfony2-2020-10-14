b42380a4dd8abdf3cebfe8d83fee8c6bc5d4dc19
hseka <sekaherve@gmail.com>
2020-11-30 00:31:46 +0100

    Sauvegarder en Base de données d'un nouvel Utilisateur et de son Mot de passe crypté

---
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,34 @@
+<?php
+namespace Yoda\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Yoda\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUsers implements FixtureInterface, ContainerAwareInterface
+{
+    private $container;
+
+    public function  load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('marco');
+        $user->setPassword($this->encodePassword($user, 'userpass'));
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+    public function encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+}
