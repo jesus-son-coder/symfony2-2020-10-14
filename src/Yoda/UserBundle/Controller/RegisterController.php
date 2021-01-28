affe7971876ecdac66bdb128d80437e6894a3a19
hseka <sekaherve@gmail.com>
2021-01-28 22:07:50 +0100

    Register Form

---
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     * @Template
+     */
+    public function registerAction()
+    {
+        $form = $this->createFormBuilder()
+            ->add('username','text')
+            ->add('email','email')
+            ->add('password','repeated', array(
+              'type' => 'password',
+            ))
+            ->getForm()
+        ;
+
+        return array('form' => $form->createView());
+    }
+}
