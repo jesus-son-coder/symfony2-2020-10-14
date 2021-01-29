9c3801a3fea3e0f77675070ccce944497a918647
hseka <sekaherve@gmail.com>
2021-01-29 01:29:32 +0100

    Encoding the User’s Password

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Yoda\UserBundle\Entity\User;
 
 class RegisterController extends Controller
@@ -33,8 +34,23 @@ class RegisterController extends Controller
           $user = new User();
           $user->setUsername($data['username']);
           $user->setEmail($data['email']);
+          $user->setPassword($this->encodePassword($user, $data['password']));
+
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($user);
+          $em->flush();
+
+          return $this->redirectToRoute('event');
         }
 
         return array('form' => $form->createView());
+    }
+
+
+    public function encodePassword(User $user, $plainPassword)
+    {
+      $encoder = $this->container->get('security.encoder_factory')->getEncoder($user);
+
+      return $encoder->encodePassword($plainPassword, $user->getSalt());
     }
 }
